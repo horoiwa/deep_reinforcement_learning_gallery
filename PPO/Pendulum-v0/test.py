cbe2cd78556918f72b946356ab8f7139f355cb5a
horoiwa <horoiwa195@gmail.com>
2020-09-08 01:29:15 +0900

    Dev

--- PPO/Pendulum-v0/test.py
+++ PPO/Pendulum-v0/test.py
@@ -1,8 +1,32 @@
 import ray
 
 
+@ray.remote
+class Counter(object):
+    def __init__(self):
+        self.x = 0
+
+    def inc(self):
+        self.x += 1
+
+    def get_value(self):
+        return self.x
+
+
 def main():
-    print("Hello")
+    ray.init()
+    # Create an actor process.
+    c = Counter.remote()
+
+    # Check the actor's counter value.
+    print(ray.get(c.get_value.remote()))  # 0
+
+    # Increment the counter twice and check the value again.
+    c.inc.remote()
+    c.inc.remote()
+    print(ray.get(c.get_value.remote()))  # 2
+
+    ray.shutdown()
 
 
 if __name__ == "__main__":
