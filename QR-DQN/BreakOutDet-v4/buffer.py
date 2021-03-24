44c4df54f69a316b608ac697a6732129dc5cfc53
horoiwa <horoiwa195@gmail.com>
2021-03-24 22:56:59 +0900

    dev

---
+++ QR-DQN/BreakOutDet-v4/buffer.py
@@ -0,0 +1,84 @@
+from dataclasses import dataclass
+import ray
+
+import numpy as np
+import pickle
+import zlib
+
+
+@dataclass
+class Experience:
+
+    state: np.ndarray
+
+    action: float
+
+    reward: float
+
+    next_state: np.ndarray
+
+    done: bool
+
+
+class ReplayBuffer:
+    """見通しのよさのためにRAMを無駄遣いする実装なのでせめて圧縮する
+    """
+
+    def __init__(self, max_len, compress=True):
+
+        self.max_len = max_len
+
+        self.buffer = []
+
+        self.compress = compress
+
+        self.count = 0
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def push(self, exp):
+
+        if self.compress:
+            exp = zlib.compress(pickle.dumps(exp))
+
+        if self.count == self.max_len:
+            self.count = 0
+
+        try:
+            self.buffer[self.count] = exp
+        except IndexError:
+            self.buffer.append(exp)
+
+        self.count += 1
+
+    def get_minibatch(self, batch_size):
+
+        N = len(self.buffer)
+
+        indices = np.random.choice(
+            np.arange(N), replace=False, size=batch_size)
+
+        if self.compress:
+            selected_experiences = [
+                pickle.loads(zlib.decompress(self.buffer[idx])) for idx in indices]
+        else:
+            selected_experiences = [self.buffer[idx] for idx in indices]
+
+        states = np.vstack(
+            [exp.state for exp in selected_experiences]).astype(np.float32)
+
+        actions = np.vstack(
+            [exp.action for exp in selected_experiences]).astype(np.float32)
+
+        rewards = np.array(
+            [exp.reward for exp in selected_experiences]).reshape(-1, 1)
+
+        next_states = np.vstack(
+            [exp.next_state for exp in selected_experiences]
+            ).astype(np.float32)
+
+        dones = np.array(
+            [exp.done for exp in selected_experiences]).reshape(-1, 1)
+
+        return (states, actions, rewards, next_states, dones)
---
+++ QR-DQN/ref.md
@@ -0,0 +1,6 @@
+
+Distributional Reinforcement Learning with Quantile Regression
+
+https://arxiv.org/pdf/1710.10044.pdf
+
+https://biolab.sakura.ne.jp/nhk-quartile.html
