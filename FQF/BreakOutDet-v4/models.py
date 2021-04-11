a22652b3f2e587e0f1fe5b5af3fdf8c28e80ecd3
horoiwa <horoiwa195@gmail.com>
2021-04-11 23:19:33 +0900

    dev

--- FQF/BreakOutDet-v4/models.py
+++ FQF/BreakOutDet-v4/models.py
@@ -1,3 +1,5 @@
+import random
+
 import numpy as np
 import tensorflow as tf
 import tensorflow.keras.layers as kl
@@ -30,46 +32,60 @@ class FQFNetwork(tf.keras.Model):
             num_quantiles, self.quatile_embedding_dim, self.state_embedding_dim)
 
         self.quantile_function_layer = QuantileFunctionNetwork(
-            self.action_space, self.num_quantiles, self.state_embedding_dim)
+            self.action_space, self.state_embedding_dim, self.quatile_embedding_dim)
 
-    def call(self, x, quantiles=None):
+    def call(self, x, given_quantiles_all=None):
+        """
+            Note:
+
+            quantiles_all: τ = 0, ..., 1.0
+                           (length==num_quantiles+1)
+            quantiles: Excluding τ=0,1 from quantiles_all,
+                       (length==num_quantiles-1)
+            quantiles_hat: τ^_{i} = (τ_{i} + τ_{i+1}) / 2
+                           (length==num_quantiles)
+        """
 
         batch_size = x.shape[0]
 
         state_embedded = self.state_embedding_layer(x, batch_size)
         assert state_embedded.shape[1] == self.state_embedding_dim
 
-        """
-            quantiles: 提案分位点からτ=0,1を除外したもの.
-                       shape[1] == num_quantiles-1
-            quantiles_hat: 提案分位点の中間点. τ^_{i} = (τ{i} + τ_{i+1}) / 2
-                       shape[1] == num_quantiles
+        if given_quantiles_all:
+            quantiles_all = given_quantiles_all
+        else:
+            quantiles_all = self.fraction_proposal_layer(state_embedded)
 
-            E[Z(s, a)]を計算するだけならquantiles_hatだけでOK
-        """
-        (quantiles, quantiles_embedded,
-         quantiles_hat, quantiles_hat_embedded,
-         quantile_hat_probs) = self.fraction_proposal_layer(state_embedded, batch_size)
-
-        quantile_values = self.quantile_function_layer(
-            state_embedded, quantiles_embedded, batch_size)
+        quantiles_hat = (quantiles_all[:, 1:] + quantiles_all[:, :-1]) / 2.  # (batch_size, num_quantiles)
+        quantile_hat_probs = quantiles_all[:, 1:] - quantiles_all[:, :-1]
 
         quantile_hat_values = self.quantile_function_layer(
-            state_embedded, quantiles_hat_embedded, batch_size)
+            state_embedded, quantiles_hat)
 
-        quantile_loss = None
-
-        return quantiles_hat, quantiles_hat_values, quantiles_loss
+        return (quantile_hat_values, quantile_hat_probs)
 
     def sample_action(self, x, epsilon):
 
-        if np.random.random() > epsilon:
+        if random.random() > epsilon:
             selected_actions, _ = self.sample_actions(x)
             selected_action = selected_actions[0][0].numpy()
         else:
             selected_action = np.random.choice(self.action_space)
 
         return selected_action
+
+    def sample_actions(self, state):
+        quantile_hat_values, quantile_hat_probs = self(state)
+        quantile_hat_probs = tf.repeat(
+            tf.expand_dims(quantile_hat_probs, axis=1),
+            self.action_space, axis=1)
+
+        weighted_quantile_hat_values = quantile_hat_values * quantile_hat_probs
+        q_means = tf.reduce_mean(
+            weighted_quantile_hat_values, axis=2, keepdims=True)
+        selected_actions = tf.argmax(q_means, axis=1)
+
+        return selected_actions, quantile_hat_values
 
 
 class StateEmbeddingNetwork(tf.keras.layers.Layer):
@@ -110,10 +126,9 @@ class FractionProposalNetwork(tf.keras.layers.Layer):
         self.dense_1 = kl.Dense(num_quantiles, activation=None,
                                 kernel_initializer="he_normal")
 
-        self.dense_2 = kl.Dense(state_embedding_dim, activation="relu",
-                                kernel_initializer="he_normal")
+    def call(self, state_embedded):
 
-    def call(self, state_embedded, batch_size):
+        batch_size = state_embedded.shape[0]
 
         logits = self.dense_1(state_embedded)
         quantiles_excluding_zero = tf.cumsum(
@@ -121,89 +136,68 @@ class FractionProposalNetwork(tf.keras.layers.Layer):
         tau_zero = tf.zeros([batch_size, 1], dtype=tf.float32)
         quantiles_all = tf.concat([tau_zero, quantiles_excluding_zero], axis=-1)  # (batch_size, num_quantiles+1)
 
-        quantiles_hat = (quantiles_all[:, 1:] + quantiles_all[:, :-1]) / 2.  # (batch_size, num_quantiles)
-        quantile_hat_probs = quantiles_all[:, 1:] - quantiles_all[:, :-1]
-
-        """ Embed proposed quantiles excluding τ＝0, 1
-        """
-        quantiles = quantiles_all[:, 1:-1]  # (batch_size, num_quantiles-1)
-        quantiles_embedded = tf.expand_dims(quantiles, axis=2)  # (batch_size, num_quantiles-1, 1)
-        quantiles_embedded = tf.repeat(
-            quantiles_embedded, self.quantile_embedding_dim, axis=2)  # (batch_size, num_quantiles-1, quant_emb_dim)
-        #: Compute cos(iπτ), shape==(batch_size, num_quantiles-1, quant_embedding_dim)
-        pis = tf.range(1, self.quantile_embedding_dim+1, 1, dtype=tf.float32) * np.pi
-        quantiles_embedded = tf.cos(quantiles_embedded * pis)
-        #: DenseLayerに適用するための不自然なreshape
-        quantiles_embedded = tf.reshape(
-            quantiles_embedded, [batch_size * (self.num_quantiles - 1), self.quantile_embedding_dim])
-        #: (batch_size * (num_quantiles-1), quant_embed_dim)
-        quantiles_embedded = self.dense_2(quantiles_embedded)
-
-        assert quantiles_embedded.shape.as_list() == [batch_size * (self.num_quantiles-1), self.state_embedding_dim]
-
-        """ Embed quantiles_hat
-        """
-        quantiles_hat_embedded = tf.expand_dims(quantiles_hat, axis=2)  # (batch_size, num_quantiles, 1)
-        quantiles_hat_embedded = tf.repeat(
-            quantiles_hat_embedded, self.quantile_embedding_dim, axis=2)  # (batch_size, num_quantiles, quant_emb_dim)
-
-        pis = tf.range(1, self.quantile_embedding_dim+1, 1, dtype=tf.float32) * np.pi
-        quantiles_hat_embedded = tf.cos(quantiles_hat_embedded * pis)
-
-        quantiles_hat_embedded = tf.reshape(
-            quantiles_hat_embedded, [batch_size * self.num_quantiles, self.quantile_embedding_dim])
-        quantiles_hat_embedded = self.dense_2(quantiles_hat_embedded)
-
-        assert quantiles_hat_embedded.shape.as_list() == [batch_size * self.num_quantiles, self.state_embedding_dim]
-
-        return (quantiles, quantiles_embedded,
-                quantiles_hat, quantiles_hat_embedded,
-                quantile_hat_probs)
+        return quantiles_all
 
 
 class QuantileFunctionNetwork(tf.keras.layers.Layer):
 
-    def __init__(self, action_space, num_quantiles, state_embedding_dim):
+    def __init__(self, action_space, state_embedding_dim, quantile_embedding_dim):
 
         super(QuantileFunctionNetwork, self).__init__()
+
         self.action_space = action_space
-        self.num_quantiles = num_quantiles
+
         self.state_embedding_dim = state_embedding_dim
 
-        self.dense1 = kl.Dense(512, activation="relu",
-                                kernel_initializer="he_normal")
+        self.quantile_embedding_dim = quantile_embedding_dim
 
-        self.out = kl.Dense(self.num_actions
-                                kernel_initializer="he_normal")
+        self.dense1 = kl.Dense(self.state_embedding_dim, activation="relu",
+                               kernel_initializer="he_normal")
 
-    def call(self, state_embedded, quantiles_embedded, batch_size):
+        self.dense2 = kl.Dense(512, activation="relu",
+                               kernel_initializer="he_normal")
+
+        self.out = kl.Dense(self.action_space, activation=None,
+                            kernel_initializer="he_normal")
+
+    def call(self, state_embedded, quantiles):
+
+        batch_size, N = state_embedded.shape[0], quantiles.shape[1]
+
+        """ Repeat for the number of quantiles
         """
-        Note:
-            quantiles_embedded:
-              if quantiles_embedded,
-                shape == (batch_size * (num_quantiles-1), 3136)
-                N == num_quantiles - 1
-              elif quantiles_hat_embedded,
-                shape == (batch_size * num_quantiles, 3136)
-                N == num_quantiles
-        """
-        N = int(quantiles_embedded.shape[0] / batch_size)
-
         state_embedded = tf.repeat(
             tf.expand_dims(state_embedded, axis=1), N, axis=1)
         state_embedded = tf.reshape(
             state_embedded,
             [batch_size * N, self.state_embedding_dim])
-        assert state_embedded.shape == quantiles_embedded.shape
+
+        """ cosine embedding of τ
+        """
+        quantiles_embedded = tf.expand_dims(quantiles, axis=2)
+        quantiles_embedded = tf.repeat(
+            quantiles_embedded, self.quantile_embedding_dim, axis=2)
+
+        pis = tf.range(1, self.quantile_embedding_dim+1, 1, dtype=tf.float32) * np.pi
+        quantiles_embedded = tf.cos(quantiles_embedded * pis)
+
+        #: DenseLayerに適用するための不自然なreshape
+        quantiles_embedded = tf.reshape(
+            quantiles_embedded, [batch_size * N, self.quantile_embedding_dim])
+        quantiles_embedded = self.dense1(quantiles_embedded)
+
+        assert quantiles_embedded.shape.as_list() == [batch_size * N, self.state_embedding_dim]
+        assert state_embedded.shape.as_list() == [batch_size * N, self.state_embedding_dim]
 
         x = state_embedded * quantiles_embedded
-        x = self.dense1(x)
+        x = self.dense2(x)
         quantile_values = self.out(x)
 
-        import pdb; pdb.set_trace()
+        quantile_values = tf.reshape(
+            quantile_values, (batch_size, N, self.action_space))
+        quantile_values = tf.transpose(quantile_values, [0, 2, 1])
 
-        #quantile_values = tf.reshape(out, (batch_size, self.action_space, self.N))
-        return  quantile_values
+        return quantile_values
 
 
 if __name__ == "__main__":
@@ -227,6 +221,7 @@ if __name__ == "__main__":
     fqf_net = FQFNetwork(action_space=4, num_quantiles=10, quantile_embedding_dim=12)
 
     out = fqf_net(state)
+    fqf_net.sample_actions(state)
 
     #selected_actions, quantile_qvalues = qnet.sample_actions(state)
     #print(selected_actions.numpy().flatten())
