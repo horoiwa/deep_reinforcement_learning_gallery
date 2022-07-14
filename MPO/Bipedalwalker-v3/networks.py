84bd3b6b69fdd377d0a403d228f61729bf8ed7a0
horoiwa <horoiwa195@gmail.com>
2022-07-14 22:18:41 +0900

    dev

--- MPO/Bipedalwalker-v3/networks.py
+++ MPO/Bipedalwalker-v3/networks.py
@@ -1,10 +1,10 @@
-from statistics import covariance
 import numpy as np
 
 import tensorflow as tf
 import tensorflow.keras.layers as kl
 import tensorflow_probability as tfp
-from tensorflow_probability import distribution as tfd
+from tensorflow_probability import distributions as tfd
+
 
 
 class PolicyNetwork(tf.keras.Model):
@@ -18,13 +18,17 @@ class PolicyNetwork(tf.keras.Model):
 
         self.action_space = action_space
 
-        self.dense1 = kl.Dense(256, activation="relu")
+        self.dense1 = kl.Dense(256, activation="relu",
+                               kernel_initializer="Orthogonal")
 
-        self.dense2 = kl.Dense(256, activation="relu")
+        self.dense2 = kl.Dense(256, activation="relu",
+                               kernel_initializer="Orthogonal")
 
-        self.mu = kl.Dense(self.action_space, activation="tanh")
+        self.mean = kl.Dense(self.action_space, activation="tanh",
+                             kernel_initializer="Orthogonal")
 
-        self.cholesky_factor = kl.Dense(sum(range(self.action_space+1)))
+        self.cholesky_factor = kl.Dense(sum(range(self.action_space+1)),
+                               kernel_initializer="Orthogonal")
 
     #@tf.function
     def call(self, x):
@@ -33,24 +37,23 @@ class PolicyNetwork(tf.keras.Model):
 
         x = self.dense2(x)
 
-        mu = self.mu(x)
+        mean = self.mean(x)
 
         A = tfp.math.fill_triangular(
             tf.math.softplus(self.cholesky_factor(x))
         )
 
-        #: Note: tensorflow_propabilityの実装では共分散行列のupper-triangleは無視されるのでAをそのまま使ってもたぶん問題ない
-        covariance_matrix = tf.matmul(A, tf.transpose(A)) + 1e-6
+        covariance_matrix = tf.matmul(A, tf.transpose(A, perm=[0, 2, 1])) + 1e-6
 
         dist = tfd.Independent(
             tfd.MultivariateNormalFullCovariance(
-                loc=mu, covariance_matrix=covariance_matrix
+                loc=mean, covariance_matrix=covariance_matrix
                 )
             )
 
         return dist
 
-    def sample_actions(self, states):
+    def sample_action(self, states):
 
         dist = self(states)
 
@@ -65,11 +68,13 @@ class CriticNetwork(tf.keras.Model):
 
         super(CriticNetwork, self).__init__()
 
-        self.dense_1 = kl.Dense(256, activation="relu")
+        self.dense_1 = kl.Dense(256, activation="relu",
+                               kernel_initializer="Orthogonal")
 
-        self.dense_2 = kl.Dense(256, activation="relu")
+        self.dense_2 = kl.Dense(256, activation="relu",
+                               kernel_initializer="Orthogonal")
 
-        self.q = kl.Dense(1)
+        self.q = kl.Dense(1, kernel_initializer="Orthogonal")
 
     @tf.function
     def call(self, states, actions):
@@ -80,6 +85,24 @@ class CriticNetwork(tf.keras.Model):
 
         x1 = self.dense_2(x1)
 
-        q = self.q1(x1)
+        q = self.q(x1)
 
         return q
+
+
+if __name__ == "__main__":
+    import gym
+
+    env = gym.make("BipedalWalker-v3")
+
+    policy = PolicyNetwork(action_space=4)
+    critic = CriticNetwork()
+
+    dummy_state = env.reset()
+    dummy_state = (dummy_state[np.newaxis, ...]).astype(np.float32)
+
+    dummy_action = np.random.normal(0, 0.1, size=4)
+    dummy_action = (dummy_action[np.newaxis, ...]).astype(np.float32)
+
+    action = policy(dummy_state)
+    qvalue = critic(dummy_state, dummy_action)
