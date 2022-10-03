a7e0d4a34f00b03987c4c59b96b4c05dcb18c38f
horoiwa <horoiwa195@gmail.com>
2022-10-03 19:39:53 +0000

    Add model

--- CQL/main.py
+++ CQL/main.py
@@ -1,4 +1,23 @@
 
 
+class CQLAgent:
+
+    def __init__(self):
+        self._setup()
+
+    def _setup(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def update_network(self):
+        pass
+
+
+def main(n_iter=100):
+    for i in range(n_iter):
+        print(i)
+
 if __name__ == '__main__':
-    pass
+    main()
