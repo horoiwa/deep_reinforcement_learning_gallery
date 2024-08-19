620d3a6ca61a04f141e001afdc53b9014ee43d2b
horoiwa <horoiwa195@gmail.com>
2024-08-20 00:13:32 +0900

    dev

--- BiggerBetterFaster/README.md
+++ BiggerBetterFaster/README.md
@@ -4,7 +4,10 @@ Unofficial reimplementaion of [Bigger Better Faster](https://arxiv.org/abs/2305.
 
 [official implementation](https://github.com/google-research/google-research/tree/master/bigger_better_faster)
 
+## Note
+
+The official implementation uses Rainbow without NoisyNet as the base model, but for simplicity, this implementation uses QR-DQN with PER and multistep-return as the base model.
 
 ## Requirements
 
-`tensoflow==2.13.1`
+`tensoflow==2.13.1`, `gym==0.25.2`
---
+++ BiggerBetterFaster/main.py
@@ -0,0 +1,72 @@
+from pathlib import Path
+import shutil
+
+import tensoflow as tf
+import gym
+
+from buffers import PrioritizedReplayBuffer
+from networks import BBFNetwork
+
+
+class BBFAgent:
+    def __init__(self, env_id: str, logdir: Path | None):
+
+        self.env_id = env_id
+        self.action_space = gym.make(self.env_id).action_space.shape[0]
+        self.summary_writer = tf.summary.create_file_writer(str(logdir)) if logdir else None
+        self.network = BBFNetwork(action_space=self.action_space, target=False)
+        self.target_network = BBFNetwork(action_space=self.action_space, target=True)
+        self.replay_buffer = PrioritizedReplayBuffer(maxlen=None)
+        self.optimizer = tf.keras.optimizers.AdamW(lr=0.0005)
+        self.gamma = 0.997
+        self.batch_size = 32
+        self.update_period = 1
+        self.num_updates = 2
+        self.gamma = 0.99
+
+        self.setup()
+        self.global_steps = 0
+
+    def setup(self):
+        pass
+    def save(self):
+        pass
+    def load(self):
+        pass
+    def rollout(self):
+        pass
+    def update_network(self):
+        pass
+    def update_target_network(self):
+        pass
+    def test_play(self):
+        pass
+
+
+def train(env_id="BipedalWalker-v3", max_steps=100_000):
+    """
+    Note:
+        if you failed to "pip install gym[box2d]", try "pip install box2d"
+    """
+
+    LOGDIR = Path(__file__).parent / "log"
+    if LOGDIR.exists():
+        shutil.rmtree(LOGDIR)
+
+    agent = BBFAgent(env_id=env_id, logdir=LOGDIR)
+
+    episodes =  0
+    while agent.global_steps < max_steps:
+        rewards, steps = agent.rollout()
+        episodes += 1
+        steps += steps
+        print(f"Episode {episodes}: {rewards}, {agent.global_steps} steps")
+
+
+    agent.save("checkpoints/")
+    print("Training finshed")
+
+
+
+if __name__ == '__main__':
+    train()
